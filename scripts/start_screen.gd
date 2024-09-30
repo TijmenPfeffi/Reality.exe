97ce0a009ab062e2904c2819801f353ed31814be
TijmenPfeffi <162435731+TijmenPfeffi@users.noreply.github.com>
2024-09-30 16:59:35 +0200

    ad

--- scripts/start_screen.gd
+++ scripts/start_screen.gd
@@ -4,4 +4,4 @@ func _on_button_quit_pressed():
 	get_tree().quit()
 
 func _on_button_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/Startup animation.tscn")
